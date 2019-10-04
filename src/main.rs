2d554b211adbc95479dcce312a1e93164afa4cb3
Jordan Petridis <jpetridis@gnome.org>
2019-10-05 00:51:19 +0300

    add ppc64le arch

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,26 @@ fn get_sha(path: &str) -> Result<String, Error> {
 
 #[test]
 fn test_get_sha() -> Result<(), Error> {
-    let source = "4d0d64c60561ea8acc01b866f48e42c906b60215c43ff7d7b088ee21bdcd813e";
-    let x86_64 = "6e38e51cc5cc2a6e66401ed88571fb044792e0694b2af8edd9b12fbe87c4e1f9";
-    let i686 = "16107f767fe874fc870e46ffc74cfaeae670d5ff4b9b620f9867c87c26fb58f2";
-    let aarch64 = "bb8664f0aa5a5c310f0ec51bd6520539ede94a39aebdb13fd81f8658c18eec2c";
-    let armv7hf = "2cfab068b16c0e4e9450abb1826d82c55c92c0438e3c82596458884863bf2857";
+    let source = "10abffac50a729cf74cef6dd03193a2f4647541bd19ee9281be9e5b12ca8cdfd";
+    let x86_64 = "b498a84947012064363607c29cdd2ba6fab9a5260212ec4a7151dafd0d079081";
+    let i686 = "f99daecd13c716b0c285b1c26225e71df2c7ca520a00285ba4c26ff767680ecb";
+    let aarch64 = "aed553dbf457d8239eb3b01d4a2f422672772a0114aa43647294eca1b358d219";
+    let armv7hf = "d4ed33497b3a3b89f5910fa140bbc9f4865ebf1735898bbe06266ef5944f6fca";
+    let powerpc64le = "eecebeaa2100950196e604d5680f9a9e7b6e97c535c246011a2247fe6ce629fc";
 
-    let s = "https://static.rust-lang.org/dist/rustc-1.29.1-src.tar.xz";
-    let x = "https://static.rust-lang.org/dist/rust-1.29.1-x86_64-unknown-linux-gnu.tar.xz";
-    let i = "https://static.rust-lang.org/dist/rust-1.29.1-i686-unknown-linux-gnu.tar.xz";
-    let aa = "https://static.rust-lang.org/dist/rust-1.29.1-aarch64-unknown-linux-gnu.tar.xz";
-    let arm = "https://static.rust-lang.org/dist/rust-1.29.1-armv7-unknown-linux-gnueabihf.tar.xz";
+    let s = "https://static.rust-lang.org/dist/rustc-1.37.0-src.tar.xz";
+    let x = "https://static.rust-lang.org/dist/rust-1.37.0-x86_64-unknown-linux-gnu.tar.xz";
+    let i = "https://static.rust-lang.org/dist/rust-1.37.0-i686-unknown-linux-gnu.tar.xz";
+    let aa = "https://static.rust-lang.org/dist/rust-1.37.0-aarch64-unknown-linux-gnu.tar.xz";
+    let arm = "https://static.rust-lang.org/dist/rust-1.37.0-armv7-unknown-linux-gnueabihf.tar.xz";
+    let p = "https://static.rust-lang.org/dist/rust-1.37.0-powerpc64le-unknown-linux-gnu.tar.xz";
 
     assert_eq!(get_sha(s)?, source);
     assert_eq!(get_sha(x)?, x86_64);
     assert_eq!(get_sha(i)?, i686);
     assert_eq!(get_sha(arm)?, armv7hf);
     assert_eq!(get_sha(aa)?, aarch64);
+    assert_eq!(get_sha(p)?, powerpc64le);
     Ok(())
 }
 
@@ -87,6 +90,13 @@ fn main() -> Result<(), Error> {
             "aarch64",
             format!(
                 "https://static.rust-lang.org/dist/rust-{}-aarch64-unknown-linux-gnu.tar.xz",
+                version
+            ),
+        ),
+        (
+            "powerpc64le",
+            format!(
+                "https://static.rust-lang.org/dist/rust-{}-powerpc64le-unknown-linux-gnu.tar.xz",
                 version
             ),
         ),
